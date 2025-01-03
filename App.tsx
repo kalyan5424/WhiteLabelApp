d90a4dfb323abe6af36c57336154b4a46fdffa49
B0217007 <kalyan.kulkarni@airtel.com>
2025-01-03 13:20:15 +0530

    Simplified App.tsx

--- App.tsx
+++ App.tsx
@@ -5,48 +5,39 @@
  * @format
  */
 
-import React from "react";
-import type { PropsWithChildren } from "react";
+import React from "react"
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
-} from "react-native";
+} from "react-native"
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-import { name as AppName } from "./app.json";
+} from "react-native/Libraries/NewAppScreen"
+import { name as AppName } from "./app.json"
 
+const isDarkMode = false
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? "light-content" : "dark-content"}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={styles.backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
+        style={styles.backgroundStyle}
       >
         <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            backgroundColor: "#FF4567",
+            flex: 1,
           }}
         >
           <Text
@@ -62,26 +53,18 @@ function App(): React.JSX.Element {
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: "600",
+    alignSelf: "center",
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
+  backgroundStyle: {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   },
-  highlight: {
-    fontWeight: "700",
-  },
-});
+})
 
-export default App;
+export default App
