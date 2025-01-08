94e958c4352a824b49a53f981b8d9fae110f84f3
B0217007 <kalyan.kulkarni@airtel.com>
2025-01-08 12:08:53 +0530

    Implemented White labelling for iOS

--- ios/WhiteLabelApp/AppDelegate.mm
+++ ios/WhiteLabelApp/AppDelegate.mm
@@ -1,12 +1,12 @@
 #import "AppDelegate.h"
-
+#import "RNCConfig.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"WhiteLabelApp";
+  self.moduleName = [RNCConfig envFor:@"APP_NAME"];;
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
